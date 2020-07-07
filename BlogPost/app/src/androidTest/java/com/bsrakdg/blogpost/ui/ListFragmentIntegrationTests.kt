7c0564dc3cf4e2a5071993037aa58f9c129fadba
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-07-07 13:59:09 +0300

    RecyclerView testing with fake data

---
+++ BlogPost/app/src/androidTest/java/com/bsrakdg/blogpost/ui/ListFragmentIntegrationTests.kt
@@ -0,0 +1,65 @@
+package com.bsrakdg.blogpost.ui
+
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import com.bsrakdg.blogpost.R
+import com.bsrakdg.blogpost.TestBaseApplication
+import com.bsrakdg.blogpost.di.TestAppComponent
+import com.bsrakdg.blogpost.util.Constants
+import com.bsrakdg.blogpost.util.EspressoIdlingResourceRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@InternalCoroutinesApi
+@RunWith(AndroidJUnit4ClassRunner::class)
+class ListFragmentIntegrationTests : BaseMainActivityTest() {
+
+    @get:Rule
+    val espressoIdlingResourceRule = EspressoIdlingResourceRule()
+
+    @Test
+    fun isBlogListEmpty() {
+
+        // setup
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext.applicationContext as TestBaseApplication
+
+
+        val apiService = configureFakeApiService(
+            blogDataSource = Constants.EMPTY_LIST,
+            categoriesDataSource = Constants.CATEGORIES_DATA_FILENAME,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(
+            apiService = apiService,
+            application = app
+        )
+
+        injectTest(app)
+
+        // run test
+        val scenario = launchActivity<MainActivity>()
+
+        val recyclerView = onView(withId(R.id.recycler_view))
+        recyclerView.check(matches(isDisplayed()))
+
+        onView(withId(R.id.no_data_textview))
+            .check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
+    }
+
+    override fun injectTest(application: TestBaseApplication) {
+        (application.appComponent as TestAppComponent).inject(this)
+    }
+}
