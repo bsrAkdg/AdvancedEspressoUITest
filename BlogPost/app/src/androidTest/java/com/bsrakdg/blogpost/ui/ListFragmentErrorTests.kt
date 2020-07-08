4e17141ee7e729d28d4879ec3b111d5c9d551d3b
Büşra Serdaroğlu <busraaakdag@gmail.com>
2020-07-08 20:51:46 +0300

    Testing data retrieval failures (real airplane mode)

--- BlogPost/app/src/androidTest/java/com/bsrakdg/blogpost/ui/ListFragmentErrorTests.kt
+++ BlogPost/app/src/androidTest/java/com/bsrakdg/blogpost/ui/ListFragmentErrorTests.kt
@@ -9,6 +9,7 @@ import androidx.test.platform.app.InstrumentationRegistry
 import com.bsrakdg.blogpost.R
 import com.bsrakdg.blogpost.TestBaseApplication
 import com.bsrakdg.blogpost.di.TestAppComponent
+import com.bsrakdg.blogpost.ui.viewmodel.state.MainStateEvent
 import com.bsrakdg.blogpost.util.Constants
 import com.bsrakdg.blogpost.util.EspressoIdlingResourceRule
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -85,6 +86,70 @@ class ListFragmentErrorTests : BaseMainActivityTest() {
         onView(withSubstring(Constants.NETWORK_ERROR_TIMEOUT)).check(matches(isDisplayed())) // error dialog check
 
     }
+
+    @Test
+    fun isErrorDialogShown_cannotRetrieveCategories() {
+        // real test : airplane mode -> relaunch app -> unable to retrieve categories
+        // setup
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext.applicationContext as TestBaseApplication
+
+
+        val apiService = configureFakeApiService(
+            blogDataSource = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesDataSource = Constants.SERVER_ERROR_FILENAME,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(
+            apiService = apiService,
+            application = app
+        )
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>()
+
+        onView(withText(R.string.text_error)).check(matches(isDisplayed()))
+
+        onView(withSubstring(MainStateEvent.GetCategories().errorInfo())).check(matches(isDisplayed())) // error dialog check
+
+    }
+
+    @Test
+    fun isErrorDialogShown_cannotRetrieveBlogPosts() {
+        // real test : airplane mode -> relaunch app -> unable to retrieve blog posts
+        // setup
+        val app = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext.applicationContext as TestBaseApplication
+
+
+        val apiService = configureFakeApiService(
+            blogDataSource = Constants.SERVER_ERROR_FILENAME,
+            categoriesDataSource = Constants.CATEGORIES_DATA_FILENAME,
+            networkDelay = 0L,
+            application = app
+        )
+
+        configureFakeRepository(
+            apiService = apiService,
+            application = app
+        )
+
+        injectTest(app)
+
+        val scenario = launchActivity<MainActivity>()
+
+        onView(withText(R.string.text_error)).check(matches(isDisplayed()))
+
+        onView(withSubstring(MainStateEvent.GetAllBlogs().errorInfo()))
+            .check(matches(isDisplayed())) // error dialog check
+
+    }
+
 
     override fun injectTest(application: TestBaseApplication) {
         (application.appComponent as TestAppComponent).inject(this)
